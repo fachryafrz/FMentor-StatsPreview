1d5ca0cbb728397a7e691c3a50c23d176d0af48d
FachryAfrz <fachry.dwi.fd.fda@gmail.com>
2022-09-08 15:30:48 +0700

    Finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          "very-dark": "hsl(233, 47%, 7%)",
+          desaturated: "hsl(244, 38%, 16%)",
+        },
+        violet: {
+          soft: "hsl(277, 64%, 61%)",
+          hard: "hsl(280, 100%, 25%)",
+        },
+        light: {
+          "main-paragraph": "hsla(0, 0%, 100%, 0.75)",
+          "stats-heading": "hsla(0, 0%, 100%, 0.6)",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        "lexend-deca": ["'Lexend Deca'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
